572cf31a2bd578bd93f3d2d509b0afddbe7bbe85
Deepak Naidu <deepaknaidu1122@gmail.com>
2024-08-07 19:38:34 +0530

    Removed the Header

--- src/api/optimization.js
+++ src/api/optimization.js
@@ -3,20 +3,22 @@ import axios from "axios";
 /**
  * Defines the base API URL for making requests to the server.
  */
-const API_URL = "https://seo.wisdmlabs.net"
+const API_URL = "https://seo.wisdmlabs.net";
 
 export const validateUrl = async (url) => {
   console.log(`Validation URL: ${url}`);
   try {
-      const response = await axios.get(`${API_URL}/api/optimize/validate?url=${url}`);
-      return response.data.valid;
+    const response = await axios.get(
+      `${API_URL}/api/optimize/validate?url=${url}`
+    );
+    return response.data.valid;
   } catch (error) {
-      return false;
+    return false;
   }
 };
 
 export const getURL = async (url, primaryKeywords) => {
-  console.log('URL API called');
+  console.log("URL API called");
   try {
     const response = await axios.get(
       `${API_URL}/api/optimize/url?url=${url}&primaryKeywords=${primaryKeywords}`
@@ -33,160 +35,151 @@ export const getURL = async (url, primaryKeywords) => {
 };
 
 export const getTitle = async (url, primaryKeywords) => {
-  console.log('Title API called');
+  console.log("Title API called");
   try {
-      const response = await axios.get(
-          `${API_URL}/api/optimize/title?url=${url}&primaryKeywords=${primaryKeywords}`,
-          {
-              headers: {
-                  'Accept': 'application/json, text/plain, */*',
-              }
-          }
-      );
-      return response.data;
+    const response = await axios.get(
+      `${API_URL}/api/optimize/title?url=${url}&primaryKeywords=${primaryKeywords}`
+    );
+    return response.data;
   } catch (error) {
-      if (error.response) {
-          throw new Error(error.response.data.message);
-      } else {
-          throw new Error("An error occurred. Please try again.");
-      }
+    if (error.response) {
+      throw new Error(error.response.data.message);
+    } else {
+      throw new Error("An error occurred. Please try again.");
+    }
   }
 };
 
 export const getMeta = async (url, primaryKeywords) => {
-  console.log('Meta API called');
+  console.log("Meta API called");
   try {
-      const response = await axios.get(
-        `${API_URL}/api/optimize/meta?url=${url}&primaryKeywords=${primaryKeywords}`,
-        {
-          headers: {
-              'Accept': 'application/json, text/plain, */*',
-          }
-        }
-      );
-      return response.data;
+    const response = await axios.get(
+      `${API_URL}/api/optimize/meta?url=${url}&primaryKeywords=${primaryKeywords}`
+    );
+    return response.data;
   } catch (error) {
-      if (error.response) {
-        throw new Error(error.response.data.message);
-      } else {
-        throw new Error("An error occurred. Please try again.");
-      }
-  }    
+    if (error.response) {
+      throw new Error(error.response.data.message);
+    } else {
+      throw new Error("An error occurred. Please try again.");
+    }
+  }
 };
 
 export const getH1 = async (url, primaryKeywords) => {
-  console.log('H1 API called');
+  console.log("H1 API called");
   try {
-      const response = await axios.get(
-        `${API_URL}/api/optimize/h1?url=${url}&primaryKeywords=${primaryKeywords}`,
-        {
-          headers: {
-              'Accept': 'application/json, text/plain, */*',
-          }
-        }
-      );
-      return response.data;
+    const response = await axios.get(
+      `${API_URL}/api/optimize/h1?url=${url}&primaryKeywords=${primaryKeywords}`
+    );
+    return response.data;
   } catch (error) {
-      if (error.response) {
-        throw new Error(error.response.data.message);
-      } else {
-        throw new Error("An error occurred. Please try again.");
-      }
-  }    
+    if (error.response) {
+      throw new Error(error.response.data.message);
+    } else {
+      throw new Error("An error occurred. Please try again.");
+    }
+  }
 };
 
 export const getH2 = async (url, secondaryKeywords) => {
-  console.log('H2 API called');
+  console.log("H2 API called");
   try {
-      const response = await axios.get(
-        `${API_URL}/api/optimize/h2?url=${url}&secondaryKeywords=${secondaryKeywords}`);
-      return response.data;
+    const response = await axios.get(
+      `${API_URL}/api/optimize/h2?url=${url}&secondaryKeywords=${secondaryKeywords}`
+    );
+    return response.data;
   } catch (error) {
-      if (error.response) {
-        throw new Error(error.response.data.message);
-      } else {
-        throw new Error("An error occurred. Please try again.");
-      }
-  }    
+    if (error.response) {
+      throw new Error(error.response.data.message);
+    } else {
+      throw new Error("An error occurred. Please try again.");
+    }
+  }
 };
 
 export const getH3 = async (url, secondaryKeywords) => {
-  console.log('H3 API called');
+  console.log("H3 API called");
   try {
-      const response = await axios.get(
-        `${API_URL}/api/optimize/h3?url=${url}&secondaryKeywords=${secondaryKeywords}`);
-      return response.data;
+    const response = await axios.get(
+      `${API_URL}/api/optimize/h3?url=${url}&secondaryKeywords=${secondaryKeywords}`
+    );
+    return response.data;
   } catch (error) {
-      if (error.response) {
-        throw new Error(error.response.data.message);
-      } else {
-        throw new Error("An error occurred. Please try again.");
-      }
-  }    
+    if (error.response) {
+      throw new Error(error.response.data.message);
+    } else {
+      throw new Error("An error occurred. Please try again.");
+    }
+  }
 };
 
 export const getH4 = async (url, secondaryKeywords) => {
-  console.log('H4 API called');
+  console.log("H4 API called");
   try {
-      const response = await axios.get(
-        `${API_URL}/api/optimize/h4?url=${url}&secondaryKeywords=${secondaryKeywords}`);
-      return response.data;
+    const response = await axios.get(
+      `${API_URL}/api/optimize/h4?url=${url}&secondaryKeywords=${secondaryKeywords}`
+    );
+    return response.data;
   } catch (error) {
-      if (error.response) {
-        throw new Error(error.response.data.message);
-      } else {
-        throw new Error("An error occurred. Please try again.");
-      }
-  }    
+    if (error.response) {
+      throw new Error(error.response.data.message);
+    } else {
+      throw new Error("An error occurred. Please try again.");
+    }
+  }
 };
 
 export const getH5 = async (url, secondaryKeywords) => {
-  console.log('H5 API called');
+  console.log("H5 API called");
   try {
-      const response = await axios.get(
-        `${API_URL}/api/optimize/h5?url=${url}&secondaryKeywords=${secondaryKeywords}`);
-      return response.data;
+    const response = await axios.get(
+      `${API_URL}/api/optimize/h5?url=${url}&secondaryKeywords=${secondaryKeywords}`
+    );
+    return response.data;
   } catch (error) {
-      if (error.response) {
-        throw new Error(error.response.data.message);
-      } else {
-        throw new Error("An error occurred. Please try again.");
-      }
-  }    
+    if (error.response) {
+      throw new Error(error.response.data.message);
+    } else {
+      throw new Error("An error occurred. Please try again.");
+    }
+  }
 };
 
 export const getH6 = async (url, secondaryKeywords) => {
-  console.log('H6 API called');
+  console.log("H6 API called");
   try {
-      const response = await axios.get(
-        `${API_URL}/api/optimize/h6?url=${url}&secondaryKeywords=${secondaryKeywords}`);
-      return response.data;
+    const response = await axios.get(
+      `${API_URL}/api/optimize/h6?url=${url}&secondaryKeywords=${secondaryKeywords}`
+    );
+    return response.data;
   } catch (error) {
-      if (error.response) {
-        throw new Error(error.response.data.message);
-      } else {
-        throw new Error("An error occurred. Please try again.");
-      }
-  }    
+    if (error.response) {
+      throw new Error(error.response.data.message);
+    } else {
+      throw new Error("An error occurred. Please try again.");
+    }
+  }
 };
 
 export const getContent = async (url, primaryKeywords, secondaryKeywords) => {
-  console.log('Content API called');
+  console.log("Content API called");
   try {
-      const response = await axios.get(
-        `${API_URL}/api/optimize/content?url=${url}&primaryKeywords=${primaryKeywords}&secondaryKeywords=${secondaryKeywords}`);
-      return response.data;
+    const response = await axios.get(
+      `${API_URL}/api/optimize/content?url=${url}&primaryKeywords=${primaryKeywords}&secondaryKeywords=${secondaryKeywords}`
+    );
+    return response.data;
   } catch (error) {
-      if (error.response) {
-        throw new Error(error.response.data.message);
-      } else {
-        throw new Error("An error occurred. Please try again.");
-      }
-  }    
+    if (error.response) {
+      throw new Error(error.response.data.message);
+    } else {
+      throw new Error("An error occurred. Please try again.");
+    }
+  }
 };
 
 export const getImage = async (url) => {
-  console.log('Image API called');
+  console.log("Image API called");
   try {
     const response = await axios.get(
       `${API_URL}/api/optimize/image?url=${url}`
@@ -202,7 +195,7 @@ export const getImage = async (url) => {
 };
 
 export const getAnchorText = async (url) => {
-  console.log('Anchor Text API called');
+  console.log("Anchor Text API called");
   try {
     const response = await axios.get(
       `${API_URL}/api/optimize/anchorText?url=${url}`
@@ -218,7 +211,7 @@ export const getAnchorText = async (url) => {
 };
 
 export const getAnchorLinks = async (url) => {
-  console.log('Anchor Links API called');
+  console.log("Anchor Links API called");
   try {
     const response = await axios.get(
       `${API_URL}/api/optimize/anchorLink?url=${url}`
@@ -232,6 +225,3 @@ export const getAnchorLinks = async (url) => {
     }
   }
 };
-
-
-
